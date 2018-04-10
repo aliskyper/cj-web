f4128d811c9d37a9c78da6af5694e0318800656c
Quentin Tian <tiankun@ccssoft.com.cn>
2018-04-10 21:18:01 +0800

    布局调整

--- src/app/navbar.component.ts
+++ src/app/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-navbar',
